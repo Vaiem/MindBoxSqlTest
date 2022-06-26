3ca0e6549fcb6bbe1d0e3da0cddbc7cb48743477
Vaiem <valerachichkin33@gmail.com>
2022-06-26 11:14:19 +0300

    MindBoxSqlTest
    MindBoxSqlTest

---
+++ SQLQuery1MindBoxTest.sql
@@ -0,0 +1,4 @@
+﻿SELECT  [p].[Name],  [O].[Name]
+FROM [Products] AS [p]
+LEFT JOIN [productsToOder] AS [PO] ON [p].[ProductId] = [PO].[ProductId]
+LEFT JOIN [OdersDB] AS [O] ON [O].[id] = [PO].[id]
